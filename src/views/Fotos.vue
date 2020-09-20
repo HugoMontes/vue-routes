60289c654ee40776d3c9e0248e62357a8ba853bb
Hugo Montes <hugomontes927@gmail.com>
2020-09-20 19:57:31 -0400

    Rutas y botones

--- src/views/Fotos.vue
+++ src/views/Fotos.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
         <h1>Galeria de fotos</h1>
+
+        <!-- 2) Recorrer el arreglo generando botontes -->
+        <!-- en to recibe un objeto {nombreRuta, parametros} -->
+        <router-link v-for="(item, index) of fotosArreglo"
+                     :key="index"
+                     :to="{name: 'fotos', params: {id: item}}">
+            <button>foto {{ item }}</button>
+        </router-link>
+
         <!-- 3) Invocar al componente -->
         <Fotografia></Fotografia>
     </div>
@@ -13,6 +22,12 @@ export default {
     components: {
         // 2) Agregar el componente importado para usarlo como etiqueta
         Fotografia
+    },
+    // 1) Agregar un arreglo de numeros que representen nombre de una foto
+    data() {
+        return {
+            fotosArreglo: [1, 2, 3, 4] 
+        }
     }
 }
 </script>
