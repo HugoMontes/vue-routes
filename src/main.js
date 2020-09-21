85f6154f65db6753bf814b8cac7fd0e82fce9b73
Hugo Montes <hugomontes927@gmail.com>
2020-09-20 21:36:25 -0400

    Instalando bootstrap

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Importar la libreria de bootstrap
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Instalar BootstrapVue
+Vue.use(BootstrapVue)
+// Opcionalmente instalar el componente para iconos
+Vue.use(IconsPlugin)
+
+// Importar los estilos css
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
 
 new Vue({
